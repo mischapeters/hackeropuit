b72a405c47b010c2fe9546c085eedd4122085a82
Mark Janssen -- Sig-I/O Automatisering <mark@sig-io.nl>
2023-01-16 21:43:02 +0100

    Different yaml library

--- update-events.py
+++ update-events.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
 
 import sys
-import yaml
 import glob
+import ruamel.yaml as yaml
 import simplejson as json
 import yaml
 from datetime import datetime, date
 
-today=date.today()
-
-with open(r'/dev/stdin') as infile:
-    events = yaml.load( infile, Loader=yaml.SafeLoader )
-
-okevents = []
-
 def eventdate(elem):
   return elem['StartDate']
 
-# Ugly, we loop over events multiple times,
-# but I couldn't get it working in 1 pass...
+today=date.today()
+okevents = []
+
+with open("/dev/stdin") as stream:
+  try:
+    events = yaml.safe_load(stream)
+  except yaml.YAMLError as exc:
+    print(exc)
+
+#print(events)
 
 for event in events:
-  print(f"Parsing event: {event['Name']}" )
-
+  # print(f"Parsing event: {event['Name']}" )
   if event['EndDate'] < event['StartDate']:
     print(f"Disabled Event: {event['Name']}, end time before start time" )
-    events.remove(event)
+    # events.remove(event)
   elif today > event['EndDate']:
     print(f"Passed Event: {event['Name']}, already passed" )
-    events.remove(event)
+    # events.remove(event)
   else:
     print(f"Future Event: {event['Name']}, added to list" )
     okevents.append(event)
@@ -36,4 +36,3 @@ for event in events:
 okevents.sort(key=eventdate)
 output = open("events.json", "w", encoding='utf8' )
 json.dump( okevents, output, indent=4, default=str, ensure_ascii=False, encoding="utf-8")
-
